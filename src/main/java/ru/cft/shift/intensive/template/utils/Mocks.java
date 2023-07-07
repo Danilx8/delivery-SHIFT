1ad7373f8b26effb7074daaa8199a6d30e85d5e5
Ледин Даниил Александрович <daniil.ledkin@mail.ru>
2023-07-07 20:53:33 +0700

    Update mocks

--- src/main/java/ru/cft/shift/intensive/template/utils/Mocks.java
+++ src/main/java/ru/cft/shift/intensive/template/utils/Mocks.java
@@ -1,10 +1,9 @@
 package ru.cft.shift.intensive.template.utils;
 
-import ru.cft.shift.intensive.template.dto.CartDto;
-import ru.cft.shift.intensive.template.dto.ProductDescriptionDto;
-import ru.cft.shift.intensive.template.dto.ProductDto;
-import ru.cft.shift.intensive.template.dto.SpecificationsDto;
+import ru.cft.shift.intensive.template.dto.*;
+import ru.cft.shift.intensive.template.repository.ProductRepository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -12,10 +11,36 @@ public class Mocks {
     private Mocks() {}
 
     public static ProductDto products() {
-        return new ProductDto("Футболка FILA", Set.of(new SpecificationsDto("XL", "Хлопок", "Принт")));
+        return new ProductDto("Футболка FILA",  "Чёрная футболка с принтом", Set.of(new SpecificationsDto("XL", "Хлопок", "Принт")));
     }
 
-    public static ProductDescriptionDto productsDescription() {
-        return new ProductDescriptionDto("Гамбургер", "134", "Новый бургер", "149.99");
+    public static ProductDto productsDescription() {
+        return new ProductDto("Гамбургер", "134", "Новый бургер", "149.99");
     }
+
+    public static CartDto addProduct(ProductRepository productRepository) {
+        return new CartDto();
+    }
+
+
+    public static List<CartDto> cartProducts() {
+        return Arrays.asList(
+                new CartDto("29.99", "Ланч баскет 15 за 30", "Большой обед", Set.of(
+                         new CompositionDto("Здесь пишем состав", "1100г", "много")
+                ), "231")
+        );
+    }
+
+    public static DeliveryDto timeDelivery() {
+        return new DeliveryDto("13:30");
+    }
+
+    public static List<DeliveryDto> deliveryDto(DeliveryDto order) {
+        return Arrays.asList(
+                new DeliveryDto("Чебупели", Set.of(
+                        new CompositionDto("Здесь пишем состав", "1100г", "много")
+                ), "231", "Состав: тесто и ещё что-то", "100", "13:00")
+        );
+    }
+
 }
